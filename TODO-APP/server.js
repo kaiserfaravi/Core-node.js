c49b0bad62e470e8ef445ff71334c4f2099283bb
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2025-07-19 01:37:04 +0600

    creating a todo

--- TODO-APP/server.js
+++ TODO-APP/server.js
@@ -1,14 +1,51 @@
-const http = require('http')
+const http = require('http');
+const path = require('path');
+const fs = require('fs')
+
+
+const filepath = path.join(__dirname,'./DB/todo.json')
 
 const server = http.createServer((req,res)=>{
 
-   console.log(req.url,req.method);
+    const data = fs.readFileSync(filepath,{encoding:"utf-8"})
+
   
- if(req.url==='/todos' &&  req.method==='get'){
-    res.end("welcome to the to do server by me")
+ if(req.url==='/todos' &&  req.method==='GET'){
+    
+    res.writeHead(202,{
+        "content-type":"application/json",
+        
+    })
+
+    // res.setHeader("content-type","text/plain")
+    // res.setHeader("name","kaiser")
+    // res.statusCode = 303;
+
+    res.end(data)
  }
- else if(req.url ==="/todos/create-todos" &&  req.method==="POST"){
-    res.end("todo created succesfully")
+ else if(req.url ==="/todos/create-todo" &&  req.method==="POST"){
+
+   let data =''
+   req.on("data",(chunk)=>{
+      data = data+ chunk;
+
+   })
+
+   req.on("end",()=>{
+   
+      const {title,body} = JSON.parse(data);
+      const createdAt= new Date().toLocaleString()
+      const allTodos = fs.readFileSync(filepath,{encoding:"utf-8"})
+     
+      
+      const parsedAllTodos = JSON.parse(allTodos)
+
+       parsedAllTodos.push({title,body,createdAt});
+
+      fs.writeFileSync(filepath,JSON.stringify(parsedAllTodos,null,2),{encoding:"utf-8"})
+       res.end(JSON.stringify({title,body,createdAt},null,2))
+   })
+   
  }
  else{
     res.end("todo not found")
--- asyncfs.js
+++ asyncfs.js
@@ -1,4 +1,18 @@
 const fs = require('fs')
+
+
+
+const text = "learning Node js async w/r";
+
+fs.writeFile("./hello.txt",text,{encoding:"utf8"},(err,data)=>{
+    if(err){
+        console.log("ohh noo");
+    }
+})
+
+
+
+
 fs.readFile("./hello.txt",{encoding:"utf8"},(err,data)=>{
     if(err){
         console.log("something error",err);
